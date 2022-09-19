ef81dafbc6e0bb48170fdd5f0a3086ef4f8012be
Jasmine <coldlando@hotmail.com>
2022-09-19 21:29:45 +0800

    Update fadian.py

--- fadian.py
+++ fadian.py
@@ -1,4 +1,4 @@
- 
+
 from secrets import choice
 
 from json.decoder import JSONDecodeError
@@ -18,7 +18,6 @@ import random
 class Fadian(object):
     def __init__(self):
         self.data = {
-            "date": 0,
             "data": [
                 "-亲爱的{name}姐姐：近来寒暑不常，希自珍慰。在你那双又大又亮的眼睛里，我总能捕捉到你的宁静，你的热烈，你的聪颖，你的敏感。认识你以后我脑海里挂着的一切图画都消失得无影无踪，取而代之的是我俩在一起的欢乐时光。轻轻地爱你，让爱恋滋润你的心房；轻轻的想你，让思念充满我的内心；轻轻的感觉你，让温柔吹拂我的心灵；轻轻地爱你，直到永远。你身着一件紫红色旗袍，远远看去，真像一只小蝴蝶飞过一样，既美丽称身，又色彩柔和。海可以枯，石可以烂，我对你的爱，永不会变。我和你在这样茫茫的网海相遇，也许是我俩的缘吧！多少次和今天一样的夜晚，我透过黑色的天空追忆着你，那分明不在的事实显得如此美丽。似乎万物没有了生命，异常冰冷，此刻能感到温度和生命的，只有我唯一的心。我知道，我爱之所系在水之湄，情感的指向，注定我要在源头作长久的伫望。梦中的你，在眺望的下游，在情感河流的那边。也许可以重新将段段往事，牢牢捆扎成筏，在结束一段迷惘的眺望之后，沐着此时的雨雾，破釜沉舟般乘槎而来，在某个夜晚闯入你长夜孤灯的世界。一分钟就能让一个人心碎，一小时就能喜欢上一个人，一天就能爱上一个人。但是，要用一生的时间才能去忘掉一个人。",
                 "-{name}最近有很多人喜欢，这个现象不得不说惹人深思。在这个信息化的时代，人们想当然的认为媒体平台的发展能够得到更加丰富的信息量，这也意味着可以有更加全面和客观的认知，即便是隔着冰冷屏幕。但这条论断忽视了人性的因素，因为人是很容易受欲望支配的动物。举个例子，大家以为通过她的动态可以触及到她内心最为柔软的角落，全方位了解这个人。其实不然，大部分人是无法了解事物的全部的，就如同她现在对着屏幕笑，但屏幕那端的观众却无法看到躲在她桌子下面戴着项圈的我。​",
@@ -31,20 +30,22 @@ class Fadian(object):
                 "-{name}姐姐！！！！啊啊啊啊啊啊啊姐姐！！！！！！！（怒吼）（变成猩猩）（飞进原始森林）（荡树藤）（创飞路过吃香蕉的猴子）（荡树藤）（摘一个榴莲）（砸死猴王）（称霸猴群）（掌握热武器技术）（入侵人类）（猩球崛起）（称霸天下）（迎娶{name}姐姐🤤）",
                 "-感觉要为{name}姐姐守身如玉一辈子了，除了{name}姐姐其他的我都不嫁！",
                 "-我英语不好，一次英语课老师问我wife是什么意思，我向隔壁的{name}求助，她指了指自己，我愣了许久，轻轻的说：主人​",
-                "-{name}姐姐的声音就像一瓶汽水。”“你指{name}的声音就是天籁之音？”“不，我的意思是，听了姐姐的声音就像夏天里的饮料机，脸贴在玻璃上许久才选到心怡的汽水，想把仔仔细细选中的汽水打开时盖子却不小心松掉了。”“然后汽水喷涌而出？”“然后我的心就扑通扑通的涌了出去，我想把我的心送给她。”",
-            ]
+                "-{name}姐姐的声音就像一瓶汽水。”“你指{name}的声音就是天籁之音？”“不，我的意思是，听了姐姐的声音就像夏天里的饮料机，脸贴在玻璃上许久才选到心怡的汽水，想把仔仔细细选中的汽水打开时盖子却不小心松掉了。”“然后汽水喷涌而出？”“然后我的心就扑通扑通的涌了出去，我想把我的心送给她。”"
+            ],
+            "date": 0
         }
         self.api = "https://raw.githubusercontent.com/sudoskys/Fadian/main/fadian.json"
 
     async def fecthFadian(self):
-        try:
-            req = await client.get(self.api, follow_redirects=True)
-            assert req.status_code == 200
-            self.data = req.json()
-        except JSONDecodeError as e:
-            await log(f"Warning: plugin fadian failed to refresh data. {e}")
+        # try:
+        req = await client.get(self.api, follow_redirects=True)
+        assert req.status_code == 200
+        self.data = req.json()
+        # except JSONDecodeError as e:
+        # await log(f"Warning: plugin fadian failed to refresh data. {e}")
+        # finally:
         return self.data
- 
+
 
 fadianJi = Fadian()
 
@@ -66,11 +67,16 @@ async def chitang(message: Message):
     global fadianJi
     if not fadianJi.data:
         pass
-        #await fadianJi.fecthFadian()
+        # await fadianJi.fecthFadian()
     query = message.arguments
     if not query:
         return await message.edit("请指定发电对象")
     else:
-        all=await fadianJi.fecthFadian().get("data") # fadianJi.data.get("data")
-        random.shuffle(all)
-        return await message.edit(choice(all).format(name=query))
+        try:
+            org = await fadianJi.fecthFadian()  # fadianJi.data.get("data")
+            all = org.get("data")
+            random.shuffle(all)
+        except Exception as e:
+            return await message.edit(f'错误:{e}')
+        else:
+            return await message.edit(choice(all).format(name=query))
